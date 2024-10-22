59f5972a562067ae64c7b400e06ac01b01a9d573
Abdullox <yakubovabdulloh10@gmail.com>
2024-10-22 22:59:28 +0500

    made comments at critical points and added a file that explains what the task actually was

--- Day.java
+++ Day.java
@@ -11,8 +11,9 @@ public class Day{
         this.day = _day;
     }
 
+    // Return type is Day since we are returning an object of type Day
     public Day findEasterDay(int _year){
-        // start of the algorithm
+        // Start of the algorithm
         int a = _year % 19;
         int b = _year / 100;
         int c = _year % 100;
@@ -28,13 +29,15 @@ public class Day{
         int easterMonth = (h + l - 7 * m + 114) / 31; // [3=March, 4=April]
         int p = (h + l - 7 * m + 114) % 31;
         int easterDate = p + 1; // (date in Easter Month)
-        // end of the algorithm
+        // End of the algorithm
 
-        // Returning a new object of Type Day
+        // Returning a new object of type Day that takes three arguments: _year which is the param of this method which in turn takes the year that the user inputs as an argument; easterMonth and easterDate which are the values calculated in this method.
         return new Day(_year, easterMonth, easterDate); 
     }
 
+    // Overriding the default toString() method so that when we call an intance of Day class, we will get a human readable string instead of the memory location of that object in RAM.
     public String toString(){
+        // Using simple ternary operator to store the name of the month
         String easterMonthName = month == 3 ? "March" : "April";
         return "Easter in " + this.year + " falls on the " + this.day + "th of " + easterMonthName;
     }
@@ -44,9 +47,12 @@ public class Day{
         System.out.print("Enter the year: ");
         int year = src.nextInt();
 
+        // Creating an object of type Day. We only need this object so that we can call the findEasterDay() method. We need an object to be able to call this method. It's arguments do not matter at all which is why we simply pass on 0's.
         Day easterDay = new Day(0, 0, 0);
+        // Since the findEasterDay() method returns a new object of type Day and we need to store it in a variable to be able to use it later somewhere in the code, we create a variable of type Day.
         Day easter = easterDay.findEasterDay(year);
-        
+        // Printing the object to the user. There are two ways to print the result. We can either make the findEasterDay() method static and call it through the class directly without creating objects off of that class or we can keep the method non-static and call the method on an object.
+        // System.out.println(Day.findEasterDay(year));
         System.out.println(easter);
     }
 }
