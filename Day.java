7546d3c7e423edf5facd920485798270463c0071
Abdullox <yakubovabdulloh10@gmail.com>
2024-10-22 14:01:15 +0500

    modified the code: findEasterDay now returns an object of Day type, and toString method was also added so that when we print an object we actually get a human readable text; added fields and constructors as well

--- Day.java
+++ Day.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 
 public class Day{
-    public static void main(String[] args){
-        Scanner src = new Scanner(System.in);
-        System.out.println("Enter the year: ");
-        int year = src.nextInt();
+    private int year;
+    private int month;
+    private int day;
 
-        int a = year % 19;
-        int b = year / 100;
-        int c = year % 100;
+    public Day(int _year, int _month, int _day){
+        this.year = _year;
+        this.month = _month;
+        this.day = _day;
+    }
+
+    public Day findEasterDay(int _year){
+        // start of the algorithm
+        int a = _year % 19;
+        int b = _year / 100;
+        int c = _year % 100;
         int d = b / 4;
         int e = b % 4;
         int f = (b + 8) / 25;
@@ -19,23 +26,27 @@ public class Day{
         int l = (32 + 2 * e + 2 * i - h - k) % 7;
         int m = (a + 11 * h + 22 * l) / 451;
         int easterMonth = (h + l - 7 * m + 114) / 31; // [3=March, 4=April]
-        String month = null;
-        switch(easterMonth){
-            case 1: month = "January"; break;
-            case 2: month = "February"; break;
-            case 3: month = "March"; break;
-            case 4: month = "April"; break;
-            case 5: month = "May"; break;
-            case 6: month = "June"; break;
-            case 7: month = "July"; break;
-            case 8: month = "August"; break;
-            case 9: month = "September"; break;
-            case 10: month = "October"; break;
-            case 11: month = "November"; break;
-            case 12: month = "December"; break;
-        }
         int p = (h + l - 7 * m + 114) % 31;
-        int easterDate = p + 1;    // (date in Easter Month)
-        System.out.println("Easter on this year falls on the " + easterDate + "th of " + month);
+        int easterDate = p + 1; // (date in Easter Month)
+        // end of the algorithm
+
+        // Returning a new object of Type Day
+        return new Day(_year, easterMonth, easterDate); 
+    }
+
+    public String toString(){
+        String easterMonthName = month == 3 ? "March" : "April";
+        return "Easter in " + this.year + " falls on the " + this.day + "th of " + easterMonthName;
+    }
+
+    public static void main(String[] args){
+        Scanner src = new Scanner(System.in);
+        System.out.print("Enter the year: ");
+        int year = src.nextInt();
+
+        Day easterDay = new Day(0, 0, 0);
+        Day easter = easterDay.findEasterDay(year);
+        
+        System.out.println(easter);
     }
 }
